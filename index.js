0bcd6b8fd001fb0fedd00584410230bd4579dd87
Dakota Heim <kotieheim@gmail.com>
2019-08-29 12:00:56 -0600

    added blank functions to index.js

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const searchURL = 'https://api.edamam.com/search';
+const searchUrl = 'https://api.edamam.com/search';
+const apiKey = 'dfdeff1d32e1493b08689cc95a11b623';
+const apiId = 'e4a09154';
 
-function getDataFromApi(searchTerm, callBack) {
-    const query = {
-        q: `${searchTerm}`,
-        app_id: 'e4a09154',
-        app_key: 'dfdeff1d32e1493b08689cc95a11b623',
-        from: 0,
-        to: 6
-    };
+function watchform() {
+
+}
+
+
+function resetForm() {
+
+}
+
+function formatQueryParams(params, excludes) {
+    const queryItems = Object.keys(params)
+    .map(key => `${endoceURIComponent(key)}=${encodeURIComponent(params[key])}`);
+}
+
+
+function excIngred() {
+
+}
+
+function searchRecipes(search, excludes) {
+
+}
+
+function displayResults(responseJson) {
+    console.log(response.json);
+}
+
+function getRecipes() {
+
+}
+
+function displayDetails() {
+
+}
+
+function makeIngredientList(ingredientArr) {
+
+}
+
+function returnToList() {
     
 }
+
+$(function() {
+    
+})
